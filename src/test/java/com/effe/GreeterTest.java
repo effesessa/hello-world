158e2ac8099f45bcc81013a742c20e51823fe950
effesessa <effe.sessa@gmail.com>
2021-01-17 17:53:51 +0100

    reset

--- src/test/java/com/effe/GreeterTest.java
+++ src/test/java/com/effe/GreeterTest.java
@@ -1,20 +1,25 @@
 package com.effe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GreeterTest {
 
+    private Greeter greeter;
+
+    @BeforeEach
+    public void setUp() {
+        greeter = new Greeter();
+    }
     @Test
     public void testGreetFabrizio() {
-        Greeter greeter = new Greeter();
         assertEquals("Hello Fabrizio", greeter.greet("Fabrizio"));
     }
 
     @Test
     public void testGreetWorld() {
-        Greeter greeter = new Greeter();
         assertEquals("Hello World", greeter.greet("World"));
     }
 }
