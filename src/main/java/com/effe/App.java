23e706d7a44077175facad9800cfe214dc9b474d
effesessa <effe.sessa@gmail.com>
2021-01-17 15:48:30 +0100

    added app main

---
+++ src/main/java/com/effe/App.java
@@ -0,0 +1,11 @@
+package com.effe;
+
+public class App {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        String msg = greeter.greet("App");
+        System.out.println(msg);
+
+    }
+}
